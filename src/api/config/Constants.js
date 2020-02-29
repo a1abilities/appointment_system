2a33d11873d833ed6e6164fc549e091de178f6ec
sktanwar2014 <sktanwar.2014@gmail.com>
2020-02-29 16:10:27 +0530

    dasf

--- src/api/config/Constants.js
+++ src/api/config/Constants.js
@@ -1,8 +1,8 @@
 
-export const API_URL = 'http://appointment.a1abilities.co.nz'; // Config[KEY].API_URL;
-export const AUTH_URL = 'http://appointment.a1abilities.co.nz'; // Config[KEY].AUTH_URL;
-export const API_CONSUMER = 'http://appointment.a1abilities.co.nz'; // Config[KEY].API_URL;
+// export const API_URL = 'http://appointment.a1abilities.co.nz'; // Config[KEY].API_URL;
+// export const AUTH_URL = 'http://appointment.a1abilities.co.nz'; // Config[KEY].AUTH_URL;
+// export const API_CONSUMER = 'http://appointment.a1abilities.co.nz'; // Config[KEY].API_URL;
 
-// export const API_URL = 'http://localhost:5000'; // Config[KEY].API_URL;
-// export const AUTH_URL = 'http://localhost:5000'; // Config[KEY].AUTH_URL;
-// export const API_CONSUMER = 'http://localhost:5000'; // Config[KEY].API_URL;
+export const API_URL = 'http://localhost:5000'; // Config[KEY].API_URL;
+export const AUTH_URL = 'http://localhost:5000'; // Config[KEY].AUTH_URL;
+export const API_CONSUMER = 'http://localhost:5000'; // Config[KEY].API_URL;
