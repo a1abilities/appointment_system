632e4dd504b65f6d728fc2d9ca7626433db74202
sktanwar2014 <sktanwar.2014@gmail.com>
2020-05-29 16:36:13 +0530

    change api

--- server/lib/databaseMySQL.js
+++ server/lib/databaseMySQL.js
@@ -1,12 +1,12 @@
 const env = 'local';
 let DbName = 'a1abiliti_fme';
-let domainName = 'appoint.a1abilities.co.nz';
+let domainName = 'appointment.a1abilities.co.nz';
 
 console.log('env db', env);
 
 if (env === 'prod') {
   DbName = 'a1abiliti_fme';
-  domainName = 'appoint.a1abilities.co.nz'
+  domainName = 'appointment.a1abilities.co.nz';
 } else {  
   DbName = 'a1abiliti_fme'
   domainName = 'localhost:3000'
--- src/api/config/Constants.js
+++ src/api/config/Constants.js
@@ -1,16 +1,16 @@
-// export const API_URL = 'http://appointment.a1abilities.co.nz'; // Config[KEY].API_URL;
-// export const AUTH_URL = 'http://appointment.a1abilities.co.nz'; // Config[KEY].AUTH_URL;
-// export const API_CONSUMER = 'http://appointment.a1abilities.co.nz'; // Config[KEY].API_URL;
+export const API_URL = 'http://fme.a1abilities.co.nz/'; // Config[KEY].API_URL;
+export const AUTH_URL = 'http://fme.a1abilities.co.nz/'; // Config[KEY].AUTH_URL;
+export const API_CONSUMER = 'http://fme.a1abilities.co.nz/'; // Config[KEY].API_URL;
 
 
 
-// export const API_URL = 'http://appoint.a1abilities.co.nz'; // Config[KEY].API_URL;
-// export const AUTH_URL = 'http://appoint.a1abilities.co.nz'; // Config[KEY].AUTH_URL;
-// export const API_CONSUMER = 'http://appoint.a1abilities.co.nz'; // Config[KEY].API_URL;
+// export const API_URL = 'http://appointment.a1abilities.co.nz/'; // Config[KEY].API_URL;
+// export const AUTH_URL = 'http://appointment.a1abilities.co.nz/'; // Config[KEY].AUTH_URL;
+// export const API_CONSUMER = 'http://appointment.a1abilities.co.nz/'; // Config[KEY].API_URL;
 
-export const API_URL = 'http://localhost:5000'; // Config[KEY].API_URL;
-export const AUTH_URL = 'http://localhost:5000'; // Config[KEY].AUTH_URL;
-export const API_CONSUMER = 'http://localhost:5000'; // Config[KEY].API_URL;
+// export const API_URL = 'http://localhost:5000'; // Config[KEY].API_URL;
+// export const AUTH_URL = 'http://localhost:5000'; // Config[KEY].AUTH_URL;
+// export const API_CONSUMER = 'http://localhost:5000'; // Config[KEY].API_URL;
 
 // console.log("process.env.NODE_ENV",KEY);
 
